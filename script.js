b8c694333ece6b9a87337c592c073235aefa3674
misaae1 <129649205+misaae1@users.noreply.github.com>
2026-03-07 01:38:19 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function openHeart(){
+
+document.getElementById("startScreen").style.display = "none";
+document.getElementById("mainContent").style.display = "block";
+
+}
+
+console.clear();
+
+/* SETUP */
+const scene = new THREE.Scene();
